5abbb80d36e9c65ccb6e15444a91eaf3d9029795
Felicia Carita <feliscarita1@gmail.com>
2026-06-17 19:31:15 +0700

    Update NoteController.php

--- mynotes/app/Http/Controllers/NoteController.php
+++ mynotes/app/Http/Controllers/NoteController.php
@@ -8,7 +8,6 @@ use Illuminate\Support\Facades\Auth;
 
 class NoteController extends Controller
 {
-    // ─── Dashboard / Index ────────────────────────────────────────────────────
 
     public function index()
     {
